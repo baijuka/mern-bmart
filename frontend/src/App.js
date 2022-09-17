5f716836f2fbf4491e22956c2a8c72db34d48a44
Baiju Abraham <baijuka@yahoo.com>
2022-09-18 00:20:42 +0100

    Add images and data.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import data from "./data";
 
 function App() {
   return (
@@ -6,7 +7,23 @@ function App() {
         <a href='/'>B-mart</a>
       </header>
       <main>
-        Product list
+        <h1>Featured Products</h1>
+        <div className='products'>
+          {data.products.map(product => (
+            <div className='product' key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className='product-info'>
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p><strong>£{product.price}</strong></p>
+                <button>Add to bag</button>
+              </div>
+            </div>
+          ))}
+        </div>
       </main>
     </div>
   );
