5b2cf9b89752411ddf68412611989eee2178d5f0
Baiju Abraham <baijuka@yahoo.com>
2022-09-17 23:29:06 +0100

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href='/'>B-mart</a>
+      </header>
+      <main>
+        Product list
+      </main>
+    </div>
+  );
+}
+
+export default App;
