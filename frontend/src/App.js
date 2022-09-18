4066267fef88e00431703cb60ff9c84f19f63a08
Baiju Abraham <baijuka@yahoo.com>
2022-09-18 23:20:41 +0100

    Add Product and Rating components and Bootstrap applied

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,37 @@
-import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import {LinkContainer} from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
-    <div>
+    <div className='d-flex flex-column site-container'>
       <header>
-        <Link to='/'>B-mart</Link>
+        <Navbar bg='dark' variant='dark'>
+          <Container>
+            <LinkContainer to='/'>
+              <Navbar.Brand>B-mart</Navbar.Brand>
+            </LinkContainer>
+          </Container>
+        </Navbar>
+        
       </header>
       <main>
-        <Routes>
-          <Route path="/" element={<HomeScreen />} />
-          <Route path="/product/:slug" element={<ProductScreen />} />
-        </Routes>
+        <Container>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/product/:slug" element={<ProductScreen />} />
+          </Routes>
+        </Container>
       </main>
+      <footer>
+        <div className='text-center'>
+          Copy right 2022 &copy;
+        </div>
+      </footer>
     </div>
     </BrowserRouter>
   );
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,9 +1,12 @@
 import React, { useReducer } from 'react'
 import { useEffect } from 'react'
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
+// import { useState } from 'react'
+
 import axios from 'axios';
 import logger from 'use-reducer-logger';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Product from '../components/Product';
 // import data from '../data'
 
 const reducer = (state, action) => {
@@ -21,7 +24,7 @@ const reducer = (state, action) => {
 
 const HomeScreen = () => {
   
-    const [{loading, error, products}, dispatch] = useReducer (logger(reducer), {
+    const [{loading, error, products}, dispatch] = useReducer(logger(reducer), {
         products : [],
         loading: true, 
         error:''
@@ -48,20 +51,15 @@ const HomeScreen = () => {
         <div className='products'>
           {
           loading ? (<div>Loading</div>) : error ? (<div>{error}</div>) : 
-          (products.map(product => (
-            <div className='product' key={product.slug}>
-              <Link to={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </Link>
-              <div className='product-info'>
-                <Link to={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </Link>
-                <p><strong>£{product.price}</strong></p>
-                <button>Add to bag</button>
-              </div>
-            </div>)
-          ))}
+          (
+            <Row>
+            {products.map(product => (
+            <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+                <Product product={product} />
+            </Col>
+            ))}
+          </Row>
+          )}
         </div>
     </div>
   )
