8026331d8443d7aa1ae719277b272cf1b01b3317
Baiju Abraham <baijuka@yahoo.com>
2022-09-23 23:44:31 +0100

    Add OrderScreen for order review

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,6 +18,7 @@ import ShippingAddressScreen from './screens/ShippingAddressScreen';
 import SignupScreen from './screens/SignupScreen';
 import PaymentMethodScreen from './screens/PaymentMethodScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import OrderScreen from './screens/OrderScreen';
 
 function App() {
 
@@ -87,7 +88,7 @@ function App() {
             <Route path="/shipping" element={<ShippingAddressScreen />} />
             <Route path="/payment" element={<PaymentMethodScreen />} />
             <Route path="/placeorder" element={<PlaceOrderScreen />} />
-
+            <Route path="/order/:id" element={<OrderScreen />}></Route>      
           </Routes>
         </Container>
       </main>
