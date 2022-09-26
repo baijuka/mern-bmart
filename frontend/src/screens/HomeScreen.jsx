843c44162c22e8d5ca14aaf6a70620bb9f33bbc1
Baiju Abraham <baijuka@yahoo.com>
2022-09-26 10:11:36 +0100

    Uninstalled use-reducer-logger

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -3,7 +3,7 @@ import { useEffect } from 'react'
 // import { useState } from 'react'
 
 import axios from 'axios';
-import logger from 'use-reducer-logger';
+// import logger from 'use-reducer-logger';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Product from '../components/Product';
@@ -27,7 +27,7 @@ const reducer = (state, action) => {
 
 const HomeScreen = () => {
   
-    const [{loading, error, products}, dispatch] = useReducer(logger(reducer), {
+    const [{loading, error, products}, dispatch] = useReducer(reducer, {
         products : [],
         loading: true, 
         error:''
